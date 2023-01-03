95a60de67769f6922af60426e22b9e77aed30e8b
sapabapakhil <121817577+sapabapakhil@users.noreply.github.com>
2023-01-03 10:28:05 +0530

    Update startup.sh

--- Question2/startup.sh
+++ Question2/startup.sh
@@ -4,7 +4,7 @@ apt-get install -y apache2 php wget
 cd /var/www/html
 rm index.html -f
 rm index.php -f
-wget https://raw.githubusercontent.com/test/question2/index.php
+wget https://github.com/sapabapakhil/test-new-code/blob/main/Question2/index.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
 sed -i "s|region-here|$REGION|" index.php
